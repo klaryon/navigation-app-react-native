ed6e59d4ebf31b6372698f51af40f27dea5b850a
Clarissa Ardon <clarissa.ardon@crealogix.com>
2022-05-14 19:40:07 +0200

    initial commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {Text} from 'react-native';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Text>App Screen with React Navigation</Text>
+    </NavigationContainer>
+  );
+};
+
+export default App;
