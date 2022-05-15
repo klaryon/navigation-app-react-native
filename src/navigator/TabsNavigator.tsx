2155f15aa5701f0ef71584542936640394c07094
Clarissa Ardon <clarissa.ardon@crealogix.com>
2022-05-16 01:09:44 +0200

    topBarIcon in each Tab.Screen

--- src/navigator/TabsNavigator.tsx
+++ src/navigator/TabsNavigator.tsx
@@ -4,6 +4,7 @@ import {Tab1Screen} from '../screens/Tab1Screen';
 import {Tab2Screen} from '../screens/Tab2Screen';
 import {StackNavigator} from './StackNavigator';
 import {colors} from '../theme/appTheme';
+import {Text} from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
@@ -16,20 +17,37 @@ export const TabsNavigator = () => {
       screenOptions={{
         headerShown: false,
         tabBarActiveTintColor: colors.primary,
+        tabBarStyle: {
+          borderTopColor: colors.primary,
+          borderTopWidth: 0,
+          elevation: 0,
+        },
+        tabBarLabelStyle: {
+          fontSize: 15,
+        },
       }}>
       <Tab.Screen
         name="Tab1"
-        options={{title: 'Tab1'}}
+        options={{
+          title: 'Tab1',
+          tabBarIcon: props => <Text style={{color: props.color}}>T1</Text>,
+        }}
         component={Tab1Screen}
       />
       <Tab.Screen
         name="Tab2"
-        options={{title: 'Tab2'}}
+        options={{
+          title: 'Tab2',
+          tabBarIcon: props => <Text style={{color: props.color}}>T2</Text>,
+        }}
         component={Tab2Screen}
       />
       <Tab.Screen
         name="StackNavigator"
-        options={{title: 'Stack'}}
+        options={{
+          title: 'Stack',
+          tabBarIcon: props => <Text style={{color: props.color}}>S1</Text>,
+        }}
         component={StackNavigator}
       />
     </Tab.Navigator>
