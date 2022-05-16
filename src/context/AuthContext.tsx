6e2546493f12bdf332bf2ab7d9baf7481bd2f290
Clarissa Ardon <clarissa.ardon@crealogix.com>
2022-05-16 05:02:10 +0200

    initiate actions - authReducer

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,5 +1,5 @@
 import React, {createContext, useReducer} from 'react';
-import { authReducer } from './authReducer';
+import {authReducer} from './authReducer';
 
 // Define how it looks, and what information I will have
 export interface AuthState {
@@ -26,14 +26,17 @@ export const AuthContext = createContext({} as AuthContextProps);
 
 // Component provider of state
 export const AuthProvider = ({children}: any) => {
-
   const [authState, dispatch] = useReducer(authReducer, authInitialState);
+
+  const signIn = () => {
+    dispatch({type: 'signIn'});
+  };
 
   return (
     <AuthContext.Provider
       value={{
         authState,
-        signIn: () => {},
+        signIn
       }}>
       {children}
     </AuthContext.Provider>
